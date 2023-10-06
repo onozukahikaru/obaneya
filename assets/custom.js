7c2a259082f2a930b4681b32fe2f1b7d1b018f07
joolen_h-onozuka <h-onozuka@joolen.com>
2023-10-06 14:58:41 +0900

    トップページ

--- assets/custom.js
+++ assets/custom.js
@@ -1,5 +1,25 @@
-    /* SP footer メニュー開閉 */
-    $('.links-footer .footer-header').on('click', function () {
-      $(this).toggleClass('opened');
-      $(this).siblings('.footer-content').toggleClass('opened');
-    })
+/* SP footer メニュー開閉 */
+$('.links-footer .footer-header').on('click', function () {
+    $(this).toggleClass('opened');
+    $(this).siblings('.footer-content').toggleClass('opened');
+})
+
+/* ranking swiper */
+const swiper = new Swiper('.ranking', {
+    direction: 'horizontal',
+    slidesPerView: 1,
+    autoplay: false,
+    loop: false,
+    centeredSlides: true,
+    breakpoints: {
+        769: {
+        slidesPerView: 4.5,
+        spaceBetween: 15,
+        centeredSlides: false,
+        }
+    },
+    navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev',
+    },
+});
