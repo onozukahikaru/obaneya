1eb40d07815de8bb1e0d0c49f5c2b2a28df76dda
joolen_h-onozuka <h-onozuka@joolen.com>
2023-11-06 14:01:50 +0900

    コードの整理

--- assets/custom.js
+++ assets/custom.js
@@ -1,25 +1,24 @@
 /* SP footer メニュー開閉 */
-$('.links-footer .footer-header').on('click', function () {
-    $(this).toggleClass('opened');
-    $(this).siblings('.footer-content').toggleClass('opened');
+$('.links-footer .footer-header').on('click', function() {
+	$(this).toggleClass('opened');
+	$(this).siblings('.footer-content').toggleClass('opened');
 })
-
 /* ranking swiper */
 const swiper = new Swiper('.ranking', {
-    direction: 'horizontal',
-    slidesPerView: 1,
-    autoplay: false,
-    loop: false,
-    centeredSlides: true,
-    breakpoints: {
-        769: {
-        slidesPerView: 4.5,
-        spaceBetween: 0,
-        centeredSlides: false,
-        }
-    },
-    navigation: {
-        nextEl: '.swiper-button-next',
-        prevEl: '.swiper-button-prev',
-    },
+	direction: 'horizontal',
+	slidesPerView: 1,
+	autoplay: false,
+	loop: false,
+	centeredSlides: true,
+	breakpoints: {
+		769: {
+			slidesPerView: 4.5,
+			spaceBetween: 0,
+			centeredSlides: false,
+		}
+	},
+	navigation: {
+		nextEl: '.swiper-button-next',
+		prevEl: '.swiper-button-prev',
+	},
 });
